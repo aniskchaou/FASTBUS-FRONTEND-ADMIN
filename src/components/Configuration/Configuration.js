a350acf4c983e109fc0f80cb86b6d5b8c36c2206
aniskchaou <kchaouanis26@gmail.com>
2020-12-07 13:10:11 +0100

    build

--- src/components/Configuration/Configuration.js
+++ src/components/Configuration/Configuration.js
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types';
 import './Configuration.css';
 
 const Configuration = () => (
-  <div className="Configuration">
-    Configuration Component
+   
+<div className="card">
+  <div className="card-header">
+    <strong className="card-title">Paramètres</strong>
   </div>
+  <div className="card-body">
+
+    
+  </div>
+</div>
 );
 
 Configuration.propTypes = {};
--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -3,8 +3,95 @@ import PropTypes from 'prop-types';
 import './DashBoard.css';
 
 const DashBoard = () => (
-  <div className="DashBoard">
-    DashBoard Component
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <strong class="card-title">Tableau de bord</strong>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="stat-widget-five">
+                  <div class="stat-icon dib flat-color-1">
+                    <i class="pe-7s-cash"></i>
+                  </div>
+                  <div class="stat-content">
+                    <div class="text-left dib">
+                      <div class="stat-text">
+                        $<span class="count">2</span>
+                      </div>
+                      <div class="stat-heading">Résérvations</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="stat-widget-five">
+                  <div class="stat-icon dib flat-color-2">
+                    <i class="pe-7s-cart"></i>
+                  </div>
+                  <div class="stat-content">
+                    <div class="text-left dib">
+                      <div class="stat-text">
+                        <span class="count">3435</span>
+                      </div>
+                      <div class="stat-heading">Destination</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="stat-widget-five">
+                  <div class="stat-icon dib flat-color-3">
+                    <i class="pe-7s-browser"></i>
+                  </div>
+                  <div class="stat-content">
+                    <div class="text-left dib">
+                      <div class="stat-text">
+                        <span class="count">122</span>
+                      </div>
+                      <div class="stat-heading">Routes</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="stat-widget-five">
+                  <div class="stat-icon dib flat-color-4">
+                    <i class="pe-7s-users"></i>
+                  </div>
+                  <div class="stat-content">
+                    <div class="text-left dib">
+                      <div class="stat-text">
+                        <span class="count">1223</span>
+                      </div>
+                      <div class="stat-heading">Voyage</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
